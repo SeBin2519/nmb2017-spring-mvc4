2c2f32bce478e92ec6aa7f03d97eff09a4643d2a
SeBinNam <nsb2519@e-mirim.hs.kr>
2017-09-20 07:49:12 +0900

    Revert "resultPage.htmㅣ 수정"
    This reverts commit 6c197f67f0367a04fff69e461e406c133016d843.

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class HelloController {
-	 @RequestMapping("/") 
-	 public String hello() {
-	   return "resultPage";
-	 }
+@ Controller
+public class HelloController { 
+   @RequestMapping("/") 
+   @ResponseBody
+   public String hello() {
+      return "Hello, world!";
+   }
 }
