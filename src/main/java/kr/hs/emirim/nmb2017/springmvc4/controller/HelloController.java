6c197f67f0367a04fff69e461e406c133016d843
SeBinNam <nsb2519@e-mirim.hs.kr>
2017-09-20 07:44:13 +0900

    resultPage.htmㅣ 수정

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -4,11 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ Controller
-public class HelloController { 
-   @RequestMapping("/") 
-   @ResponseBody
-   public String hello() {
-      return "Hello, world!";
-   }
+@Controller
+public class HelloController {
+	 @RequestMapping("/") 
+	 public String hello() {
+	   return "resultPage";
+	 }
 }
